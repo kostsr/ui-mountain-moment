ecca456edda966c59409d7799cff5402cf61a069
Konstantin <50304794+kostsr@users.noreply.github.com>
2022-03-13 21:07:31 +0300

    del: pritter

---
+++ src/views/GalleryView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="gallery">
+    <h1>This is an gallery page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GalleryView",
+  data(){
+    return {
+      albums: []
+    }
+  },
+  mounted(){
+
+  },
+  methods:{
+    async fetchAlmums(){
+      let responce = await fetch('http://localhost:62351/api/albums/')
+      if (responce.ok){
+        this.albums = await responce.json()
+      }
+    }
+  }
+};
+</script>
